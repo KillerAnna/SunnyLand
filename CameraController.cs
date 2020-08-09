57ad172237872b18ec20c7f983c0c95a2c339df1
KillerAnna <69104338+KillerAnna@users.noreply.github.com>
2020-08-10 02:35:41 +0900

    Add files via upload

---
+++ CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 player_Pos = player.transform.position;
+        transform.position = new Vector3(player_Pos.x, transform.position.y, transform.position.z);
+    }
+}
